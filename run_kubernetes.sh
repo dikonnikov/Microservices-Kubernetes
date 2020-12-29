b27f9de57036242b269ca07827020602fca54685
Dmitry Ikonnikov <d.ikonnikov@outlook.com>
2020-12-29 20:54:13 +1300

    added run_kubernetes.sh contents

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="meow13th/microkub-pro"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deployment --image=$dockerpath microkub-run
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+sleep 5
+kubectl port-forward deployment/microkub-run 8000:80
 
